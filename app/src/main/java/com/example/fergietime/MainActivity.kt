33fb150ead5d4190cd28df1ca18d872c92f3c127
N_A <kd1338248@st.kobedenshi.ac.jp>
2025-06-06 08:54:18 +0900

    コミットの実験(n_a)

--- app/src/main/java/com/example/fergietime/MainActivity.kt
+++ app/src/main/java/com/example/fergietime/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             FergieTimeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "FergieTime",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
